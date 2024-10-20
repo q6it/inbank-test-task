0f8cd98f345c3f10ff608951bba65084662bacd2
Andrei Sittsenko <andrei.sittsenko@gmail.com>
2024-10-20 23:42:01 +0300

    init setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
+  plugins: [
+    'prettier',
+  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': 'error',
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+// src/main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import './tailwind.css'; // Import Tailwind CSS
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.mount('#app');
