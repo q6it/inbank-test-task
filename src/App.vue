186d6dc855f9ea2a815079519e905938012dc1c9
Andrei Sittsenko <andrei.sittsenko@gmail.com>
2024-10-22 19:11:13 +0300

    Setup HomeView, clean up

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view />
 </template>
 
@@ -12,7 +8,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #21093a;
 }
 
 nav {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="flex flex-col justify-left px-10 py-6 md:px-10 md:py-6">
+    <img
+      alt="Vue logo"
+      src="../assets/inbank-logo.svg"
+      class="w-[113px] h-[40px] md:w-[182px] md:h-[64px]"
+    />
   </div>
+  <CalculatorComponent />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import CalculatorComponent from '@/components/CalculatorComponent.vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "HomeView",
+  name: 'HomeView',
   components: {
-    HelloWorld,
+    CalculatorComponent,
   },
 });
 </script>
